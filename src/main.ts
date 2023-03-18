3b3220b14c67e8f284574bbc028c2fd28dcac61b
S3V04KA <sevstar2206@gmail.com>
2023-03-18 10:13:23 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://178.47.46.58',
+    credentials: true
+  })
   await app.listen(3000);
 }
 bootstrap();
