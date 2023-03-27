a36211bafb4b8777e8f1da7130331372dfb1ca0a
S3V04KA <sevstar2206@gmail.com>
2023-03-28 02:04:45 +0500

    complex fix

--- src/general-resolvers/general-resolvers.controller.ts
+++ src/general-resolvers/general-resolvers.controller.ts
@@ -1,20 +1,25 @@
 import { Controller, Get, Redirect, Res, StreamableFile } from '@nestjs/common';
 import { createReadStream } from 'fs';
+import { join } from 'path';
+import { ComplexService } from 'src/complex/complex.service';
 
 @Controller('/')
 export class GeneralResolversController {
+  constructor(private readonly cService: ComplexService) {}
+
+  // @Redirect('/download-table1')
   @Get('/download-table')
-  getTable(@Res({ passthrough: true }) res): StreamableFile {
+  getTable(@Res({ passthrough: true }) res) {
+    this.cService.getComplex();
+
     const file = createReadStream('./test.xlsx');
     file.addListener('end', () => {
-      return res.redirect('/download-table1')
-    })
+      //res.status(200).send('Загрузка началась')
+    });
     res.set({
       'Content-Type': 'application/xlsx',
       'Content-Disposition': 'attachment; filename="table.xlsx',
     });
     return new StreamableFile(file);
   }
-
-  
 }
--- src/general-resolvers/general-resolvers.module.ts
+++ src/general-resolvers/general-resolvers.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GeneralResolversController } from './general-resolvers.controller';
+import { ComplexModule } from 'src/complex/complex.module';
 
 @Module({
   controllers: [GeneralResolversController],
+  imports: [ComplexModule],
 })
 export class GeneralResolversModule {}
