3678b5607a6eca2dbc413c11506e72ea7a2bdf3d
S3V04KA <sevstar2206@gmail.com>
2023-03-28 00:59:48 +0500

    mycommit

--- src/graph-ql/resolvers/graph-ql-user.resolver.ts
+++ src/graph-ql/resolvers/graph-ql-user.resolver.ts
@@ -34,7 +34,7 @@ export class GraphQLUserResolver {
   @UseGuards(new AuthGuard())
   @Query('me')
   async me(@Context('user') user: SecureUser) {
-    if(user.id == null) return new HttpException('XUi', HttpStatus.NOT_FOUND)
+    if(user.id == null) return new HttpException('no auth', HttpStatus.FORBIDDEN)
     return await this.userService.me(Number(user.id));
   }
 
